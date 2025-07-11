e32b535f2b7b1414d90455e1406fb2b5da2fadbf
dmcastro907 <dmcastro@byui.edu>
2025-07-11 14:29:44 -0600

    Character Card

---
+++ character-card/script.js
@@ -0,0 +1,41 @@
+const character = {
+      name: "Snortleblat",
+      class: "Swamp Beast Diplomat",
+      level: 5,
+      health: 100,
+      image: 'https://andejuli.github.io/wdd131/character_card/snortleblat.webp',
+      attacked() {
+        console.log(this.health)A
+        if (this.health >= 20) {
+          this.health -= 20;
+        } else {
+            alert('Character Died');
+        }
+      },
+      levelUp() {
+        this.level += 1;
+        this.health += 20;
+      }
+    };
+
+
+function render_character(){
+  const char_name = document.getElementsByClassName('name')[0];
+  char_name.innerHTML = `<p>${character.name}</p>`;
+
+  const char_class = document.getElementById('class');
+  char_class.innerHTML = `${character.class}`;
+
+  const char_level = document.getElementById('level');
+  char_level.innerHTML = `${character.level}`;
+
+  const char_health = document.getElementById('health');
+  char_health.innerHTML = `${character.health}`;
+}
+render_character();
+
+const attack = document.getElementById("attacked");
+attack.addEventListener('click', () => {character.attacked(); render_character()});
+
+const level = document.getElementById("levelup");
+level.addEventListener('click', () => {character.levelUp(); render_character()});
